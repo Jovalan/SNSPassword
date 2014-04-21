6aa29836870558a93733d487d4f14efaef692ddb
unknown <jminor@sns.com>
2014-04-21 09:36:33 -0400

    Deleted unused variables.

--- Password Gen.py
+++ Password Gen.py
@@ -8,8 +8,6 @@ import math
 import random
 
 # Set variables
-characters = ['a''b''c''d''e''f''g''h''i''j''k''l''m''n''o''p''q''r''t''u''v''w''x''y''z']
-numbers = ['1''2''3''4''5''6''7''8''9''10''11''12''13''14''15''16''17''18''19''20''21''22''23''24']
 debug = 0
 value = 0
 list = []
